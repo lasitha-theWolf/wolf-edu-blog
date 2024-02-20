8b2350f0f40a13f575193b763a31cd6e9e3d4d95
Lasitha Heenkenda <Lasithaheenkenda23@gmail.com>
2024-02-20 19:01:15 +0530

    header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,23 @@
-import { Navbar } from 'flowbite-react'
+import { Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom' 
+import {AiOutlineSearch} from 'react-icons/ai' //search icon
 
 export default function Header() {
   return (
     <Navbar className="border-b-2">
       <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
-        {/*when page minizzum logo become small*/}
+        {/*when page minizum logo become small*/}
         <span className='px-1 py-2 bg-gradient-to-r from-indigo-500 via-blue-900 to-red-500 rounded-lg text-white'>Wolf</span>
         Coder
       </Link>
+      <form>
+        <TextInput
+            type='text'
+            placeholder='search...'
+            rightIcon={AiOutlineSearch}
+       />
+      </form>
     </Navbar>
   );
 }
