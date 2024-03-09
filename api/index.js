e2e3a2cddc340ca3800b9218c98c16b5a6c1dd4a
Lasitha Heenkenda <Lasithaheenkenda23@gmail.com>
2024-03-09 23:13:57 +0530

    connect to the DB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
- import express from 'express';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
- const app = express();
+dotenv.config();
 
- app.listen(3000, () => {
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('MongoDb is connected');
+    }).catch(err => {
+        console.log(err);
+    })
+
+//mongodb+srv://lasitha:dlpodlasitha9618*@wolf-blog.dgbu0pk.mongodb.net/  oken psse kamahti namak dapn
+
+const app = express();
+
+app.listen(3000, () => {
     console.log('server is running on port 3000!');
- });
+});
 
